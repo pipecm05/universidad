83895a5649d5eb5a942e6d15828abca4fed7733e
pipecm05 <andresf.garciac1@uqvirtual.edu.co>
2024-03-02 20:24:47 -0500

    testede<curso

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -5,7 +5,9 @@ package co.edu.uniquindio.poo;
  *
  */
 public class App {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
 }
